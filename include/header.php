8dd3227589ff84bcbdd899b90b9e558a4d1dda27
SveRKeR <maxime.attala@edu.devinci.fr>
2020-09-25 16:03:35 +0200

    php + debut header

---
+++ include/header.php
@@ -0,0 +1,13 @@
+<header>
+    <div>
+        <button>
+            <i class="fas fa-bars"></i> 
+        </button>
+        <a href="#">
+            <img src="" alt="logo du site">
+        </a>
+        <button>
+            <i class="fas fa-user"></i>
+        </button>
+    </div>
+</header>
