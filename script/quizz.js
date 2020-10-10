e15dfd7601b84fe442e784e65774b58a7026c226
Maxime <maxime.attala@edu.devinci.fr>
2020-10-10 17:26:04 +0200

    :white_check_mark: finish algo for question + alert total score

--- script/quizz.js
+++ script/quizz.js
@@ -16,9 +16,18 @@ const quizz = {
                 {choice: "Fabrice", isRight: false, index: 2},
                 {choice: "Bertrand-Kamal", isRight: false, index: 3}
             ]
+        },
+        {
+            questionTitle: "Combien de pots a cassé Dorian ?",
+            answers: [
+                {choice: "3 pots", isRight: false, index: 0},
+                {choice: "4 pots", isRight: false, index: 1},
+                {choice: "5 pots", isRight: true, index: 2},
+                {choice: "6 pots", isRight: false, index: 3},
+            ]
         }
     ]
-}
+};
 
 console.log(quizz.questions.length);
 console.log(quizz.questions[1].answers.length);
@@ -30,75 +39,104 @@ let container = document.getElementById('container');
 container.appendChild(sondageTitle);
 container.appendChild(document.createElement("hr"));
 
-//création tableau
-let sondageTable = document.createElement("table");
-sondageTable.className = "table-accueil";
-let tbody = document.createElement("tbody")
-sondageTable.appendChild(tbody);
+function drawQuizz(quizz){
 
-function drawQuizz(quizz, index){
-    //création titre question
-    let tr = document.createElement("tr");
-    tbody.appendChild(tr);
-    let td = document.createElement("td");
-    tr.appendChild(td);
-    let qTitle = document.createElement("h4");
-    qTitle.className = "titleQuestion";
-    qTitle.innerHTML = quizz.questions[index].questionTitle;
-    td.appendChild(qTitle);
+    //création tableau
+    let sondageTable = document.createElement("table");
+    sondageTable.className = "table-accueil";
+    let tbody = document.createElement("tbody")
+    sondageTable.appendChild(tbody);
 
-    // création question & boutons
-    for (let j = 0; j < quizz.questions[index].answers.length; j++) {
-        
-        //div grise 100%
-        let qtr = document.createElement("tr");
-        tbody.appendChild(qtr);
-        let qtd = document.createElement("td");
-        qtr.appendChild(qtd);
-        let div100 = document.createElement("div");
-        div100.className = "hundred";
-        qtd.appendChild(div100);
+    let index = 0;
+    while(index < quizz.questions.length){
+        //création titre question
+        let tr = document.createElement("tr");
+        tr.className = index;
+        tbody.appendChild(tr);
+        let td = document.createElement("td");
+        tr.appendChild(td);
+        let qTitle = document.createElement("h4");
+        qTitle.className = "titleQuestion";
+        qTitle.innerHTML = quizz.questions[index].questionTitle;
+        td.appendChild(qTitle);
 
-        //div bleue x%
-        let divPercent = document.createElement("div");
-        divPercent.className = "percent";
-        divPercent.style.width = 100/quizz.questions[index].answers.length + "%";
-        div100.appendChild(divPercent);
+        // création question & boutons
+        for (let j = 0; j < quizz.questions[index].answers.length; j++) {
+            
+            //div grise 100%
+            let qtr = document.createElement("tr");
+            qtr.className = index;
+            tbody.appendChild(qtr);
+            let qtd = document.createElement("td");
+            qtr.appendChild(qtd);
+            let div100 = document.createElement("div");
+            div100.className = "hundred";
+            qtd.appendChild(div100);
 
-        //contenu div bleue
-        let pBlue = document.createElement("p");
-        pBlue.innerHTML = quizz.questions[index].answers[j].choice;
-        divPercent.appendChild(pBlue);
+            //div bleue x%
+            let divPercent = document.createElement("div");
+            divPercent.className = "percent";
+            divPercent.style.width = 100/quizz.questions[index].answers.length + "%";
+            div100.appendChild(divPercent);
 
-        //affichage %age a droite
-        let pPercent = document.createElement("p");
-        pPercent.innerHTML= 100/quizz.questions[index].answers.length + " %";
-        div100.appendChild(pPercent);
+            //contenu div bleue
+            let pBlue = document.createElement("p");
+            pBlue.innerHTML = quizz.questions[index].answers[j].choice;
+            divPercent.appendChild(pBlue);
 
-        //création boutton
-        let tdButton = document.createElement("td");
-        qtr.appendChild(tdButton);
-        let button = document.createElement("button");
-        button.innerHTML = "Vote";
-        button.id = quizz.questions[index].answers[j].index;
-        button.className = "answer";
-        tdButton.appendChild(button);
+            //affichage %age a droite
+            let pPercent = document.createElement("p");
+            pPercent.innerHTML= 100/quizz.questions[index].answers.length + " %";
+            div100.appendChild(pPercent);
+
+            //création boutton
+            let tdButton = document.createElement("td");
+            qtr.appendChild(tdButton);
+            let button = document.createElement("button");
+            button.innerHTML = "Vote";
+            button.id = quizz.questions[index].answers[j].index;
+            button.className = "answer";
+            tdButton.appendChild(button);
+        }
+        index++;
     }
 
     container.appendChild(sondageTable);
 }
 
 let i = 0;
+let score = 0;
+console.log("question : "+ (i+1));
+drawQuizz(quizz);
 
-drawQuizz(quizz, i);
-
-
+let linesToHide = document.getElementsByTagName("tr");
+hideQuestion(linesToHide);
 
 $('.answer').on('click', function(){
-    if (i < quizz.questions.length){
-        i++;
+    //affichage de la question suivante
+    console.log(this.id + ' ' + typeof this.id);
+    let questionIndex = parseInt(this.id);
+    console.log(questionIndex + ' ' + typeof questionIndex);
+    let linesToHide = document.getElementsByTagName("tr");
+    console.log(quizz.questions[i].answers[questionIndex].isRight);
+    //comptabilisation des points
+    if(quizz.questions[i].answers[questionIndex].isRight == true){
+        score++;
+        console.log(score);
     }
-    drawQuizz(quizz, i);
-    console.log(this.id);
-    console.log(i);
+    i++;
+    hideQuestion(linesToHide);
+    if (i >= quizz.questions.length){
+        alert("Bien joué, tu as " + score + " bonnes réponses !");
+    }
 });
+
+function hideQuestion(array){
+    for (k = 0; k < array.length; k++){
+        if(array[k].className != i){
+            array[k].style.display = "none";
+        }else{
+            linesToHide[k].style.display = "table-row";
+        }
+    }
+}
--- views/sondage.php
+++ views/sondage.php
@@ -5,69 +5,7 @@
 
 <main>
     <div id="container">
-        <!-- <h3>Sondages Koh-Lanta</h3>
-        <hr>
-        <table class="table-accueil">
-            <tbody>
-                <tr>
-                    <td>
-                        <h4 class="titleQuestion">Quelle équipe sera au conseil ?</h4>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <div class="hundred">
-                            <div class="percent p72">
-                                <p>Le Nord</p>
-                            </div>
-                            <p>72 %</p>
-                        </div>
-                    </td>
-                    <td>
-                        <button id="nord">Vote</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <div class="hundred">
-                            <div class="percent p14">
-                                <p>Le Sud</p>
-                            </div>
-                            <p>14 %</p>
-                        </div>
-                    </td>
-                    <td>
-                        <button id="sud">Vote</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <div class="hundred">
-                            <div class="percent p10">
-                                <p>L'Est</p>
-                            </div>
-                            <p>10 %</p>
-                        </div>
-                    </td>
-                    <td>
-                        <button id="est">Vote</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <div class="hundred">
-                            <div class="percent p4">
-                                <p>L'Ouest</p>                    
-                            </div>
-                            <p>4 %</p>
-                        </div>
-                    </td>
-                    <td>
-                        <button id="ouest">Vote</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table> -->
+        
     </div>
 </main>
 <script src="../script/quizz.js"></script>
